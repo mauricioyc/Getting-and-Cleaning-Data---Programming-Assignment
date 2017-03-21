77cd9dec649fc07c2d657c73448d1e6db96fe932
Mauricio <mauricio.chigutti@gmail.com>
2017-03-20 23:31:25 -0300

    fifth commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,7 @@
+## Load dplyr library
+library(dplyr)
+
+## Download data
 if(!file.exists("data.zip")){
   download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
                 destfile = "./data.zip")
